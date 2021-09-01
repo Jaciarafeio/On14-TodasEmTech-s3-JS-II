4a9929028d2d86c7382df0c27c5b3e333b8832d3
Jaciara <jaciarafeio@gmail.com>
2021-08-31 22:12:08 -0300

    Exercícios Semana 3

---
+++ Exercicios da semana3/exercicioFarmaciasemana3.js
@@ -0,0 +1,32 @@
+function desconto(){
+
+}
+
+let produto1 = prompt('Digite o nome do produto 1');
+
+let produto2 = prompt('Digite o nome do produto 2');
+
+/* coloquei as variáveis por nome de produto porque na minha visão produtos iguais têm o mesmo preço, porém, se colocasse valores nas variáveis poderiam apresentar produtos diferentes de valores iguais */
+ 
+
+if(produto1 === produto2){
+
+    alert('ATENÇÃO - desconto de R$5,00 nestes produtos');
+
+}
+
+else{
+
+    alert('Atenção - produtos sem desconto');
+
+}
+
+
+
+
+
+    
+
+
+
+
---
+++ Exercicios da semana3/exercicioVogalsemana3.js
@@ -0,0 +1,27 @@
+var letra = prompt('Insira uma letra');
+
+switch(letra) {
+
+    case 'a': alert('SIM!!!');
+        break;
+
+    case 'e': alert('SIM!!!');
+        break;
+
+    case 'i': alert('SIM!!!');
+        break;
+
+    case 'o': alert('SIM!!!');
+        break;
+
+    case 'u': alert('SIM!!!');
+        break;
+
+    
+    default: alert('NÃO!!!');
+        break;
+    
+}
+
+
+
